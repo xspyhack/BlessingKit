b197951aa79648c5ff439de03c90ffd2cdc6fc34
xspyhack <xspyhack@gmail.com>
2023-07-20 09:43:12 +0800

    fix: #0 Fix Matter model

--- Sources/Domain/Matter.swift
+++ Sources/Domain/Matter.swift
@@ -10,26 +10,26 @@ import Foundation
 /// A model that represents a matter.
 public struct Matter: Codable, Identifiable {
     /// The matter’s unique identifier.
-    public let id: String
+    public var id: String
 
     /// The title for the matter.
-    public let title: String
+    public var title: String
 
     /// The tag for the matter.
-    public let tag: Tagble
+    public var tag: Tagble
 
     /// The original occurrence date of the matter.
-    public let occurrenceDate: Date
+    public var occurrenceDate: Date
 
     /// The notes associated with the matter.
-    public let notes: String?
+    public var notes: String?
 
     /// This will be removed after Swift 5.
     public init(
         id: String,
-        title: String,
+        title: String = "",
         tag: Tagble = Tagble.random(),
-        occurrenceDate: Date,
+        occurrenceDate: Date = Date(),
         notes: String? = nil
     ) {
         self.id = id
@@ -47,12 +47,4 @@ public extension Matter {
     }
 }
 
-extension Matter: Hashable {
-    public static func == (lhs: Matter, rhs: Matter) -> Bool {
-        return lhs.id == rhs.id
-    }
-
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-}
+extension Matter: Hashable {}
